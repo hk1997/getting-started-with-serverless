eff8ec4005ed809e2232584134c6ac91df2ba5c2
dk1242 <46108847+dk1242@users.noreply.github.com>
2019-12-20 12:06:23 +0530

    Updated hello_world_dk1242.py

--- hello-world/hello_world_dk1242.py
+++ hello-world/hello_world_dk1242.py
@@ -1,36 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import json
-
-
-# In[6]:
-
 
 def lambda_handler(event, context):
     return{
         'statusCode': 200,
         'body': json.dumps('hello world')
     }
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
