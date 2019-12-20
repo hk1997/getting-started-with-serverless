ecac301ee61c40e1e98fe21acb3883f59c96c60f
Guna Shekar <gunashekherproddatoori@gmail.com>
2019-12-20 21:42:37 +0530

    add code for listing bucket objects using promises

--- list-bucket/list_bucket_GunaShekar02.js
+++ list-bucket/list_bucket_GunaShekar02.js
@@ -1,11 +1,39 @@
+// const AWS = require('aws-sdk');
+
+// exports.handler = async (event) => {
+//   AWS.config.update({region: 'ap-south-1'});
+//   let s3 = new AWS.S3();
+//   const params = {
+//     Bucket : 'guna-shekar-02',
+//   };
+//   const result = await s3.listObjects(params).promise();
+//   return result;
+// };
+
+//ABOVE CODE USES ASYNC/AWAIT PATTERN. BELOW CODE USES PROMISES.
+
 const AWS = require('aws-sdk');
 
-exports.handler = async (event) => {
+exports.handler = (event) => {
   AWS.config.update({region: 'ap-south-1'});
   let s3 = new AWS.S3();
   const params = {
     Bucket : 'guna-shekar-02',
   };
-  const result = await s3.listObjects(params).promise();
-  return result;
+  const result = s3.listObjects(params).promise();
+  return result.then(data => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+    return response;
+  })
+  .catch(err => {
+    const response = {
+      statusCode: err.statusCode,
+      body: JSON.stringify(err),
+    };
+    return response;
+  });
 };
+
