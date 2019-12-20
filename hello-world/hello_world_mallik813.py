b7f8e51c3fb0125a358822cb8f3b27def88e4d23
VIJAY07102 <44638395+VIJAY07102@users.noreply.github.com>
2019-12-20 12:01:02 +0530

    Delete hello_world_mallik813.py

--- hello-world/hello_world_mallik813.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def first_pr(event, context):
-    print("Hello World! from team UNCERTAINERS")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
