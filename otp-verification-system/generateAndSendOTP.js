d95aa583a85f46e3f8d9af01e7e3a7d8cd51accf
Guna Shekar <gunashekherproddatoori@gmail.com>
2019-12-22 18:51:30 +0530

    add retry functionality to generate otp function

--- otp-verification-system/generateAndSendOTP.js
+++ otp-verification-system/generateAndSendOTP.js
@@ -18,21 +18,29 @@ exports.handler = async (event, context) => {
     const found = await db.get(params).promise();
     if(found.Item){
       if(found.Item.expiry < Math.floor(Date.now()/1000)) {
-        await generateAndSendOTP(event.email, event.phone);
+        await generateAndSendOTP(event.email, event.phone, 0);
+        context.succeed("Done!");
         return "OTP successfully sent to your email and phone!";
+      } else if(found.Item.tries < 3) {
+        await generateAndSendOTP(event.email, event.phone, found.Item.tries+1);
+        context.succeed("Done!");
+        return "OTP successfully sent to your email and phone!";
+      } else {
+        return "You have reached the maximum limit of requesting OTPs";
       }
-      return "You have already requested for an OTP!";
     }
     else{
-      await generateAndSendOTP(event.email, event.phone);
+      await generateAndSendOTP(event.email, event.phone, 0);
+      context.succeed("Done!");
       return "OTP successfully sent to your email and phone!";
     }
   } catch(err) {
+    console.log(err);
     return "Some error occurred, please try again";
   }
 };
 
-const generateAndSendOTP = async (email,phone) => {
+const generateAndSendOTP = async (email,phone, currentTries) => {
   const OTP = Math.floor(Math.random() * 8999) + 1000;
   const otpParams = {
     TableName: 'OTPs',
@@ -41,6 +49,7 @@ const generateAndSendOTP = async (email,phone) => {
       "phone" : phone,
       "otp" : OTP,
       "expiry" : Math.floor(Date.now()/1000)+300,
+      "tries" : currentTries,
     },
   };
   const emailParams = {
@@ -64,14 +73,12 @@ const generateAndSendOTP = async (email,phone) => {
     Source: 'gunashekherproddatoori@gmail.com',
   };
   await ses.sendEmail(emailParams).promise();
-  
   await sns.setSMSAttributes({"DefaultSMSType" : "Transactional"}).promise();
   const snsParams = {
     Message: `Your OTP is ${OTP}`,
     PhoneNumber: phone,
   };
   await sns.publish(snsParams).promise();
-  context.succeed("Done!");
   
   await db.put(otpParams).promise();
 }
