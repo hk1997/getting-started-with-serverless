69772d358c0b778c954cde95ad2c8f6c29a04a53
Sarvagya Bansal <44432427+Lincoln2000@users.noreply.github.com>
2019-12-25 00:24:32 +0530

    added feature to mail the contents of a bucket

--- list-bucket/list_bucket_Lincoln2000.js
+++ list-bucket/list_bucket_Lincoln2000.js
@@ -1,11 +1,12 @@
 var AWS  = require("aws-sdk");
 const s3 = new AWS.S3();
+const ses = new AWS.SES();
 
 exports.handler = async (event)=>{
     var listjson;
     var params = {
         "Bucket": event.bucket
-    }
+    };
     try{
         listjson =  await s3.listObjectsV2(params).promise();
     }
@@ -17,5 +18,34 @@ exports.handler = async (event)=>{
     for(let i=0;i<listjson.Contents.length;i++){
         listofContents.push(listjson.Contents[i].Key);
     }
-    return listofContents;
+    var myEmail = "sarvagya60@gmail.com";
+    var charset = "UTF-8";
+
+    params = {
+        Destination: {
+         ToAddresses: [myEmail]
+        }, 
+        Message: {
+         Body: {
+          Html: {
+          Charset: charset, 
+          Data: JSON.stringify(listofContents)
+          }, 
+         }, 
+         Subject: {
+          Charset: charset, 
+          Data: "Contents of Bucket- " + event.bucket
+         }
+        },
+        Source: myEmail
+       };
+
+    var emailed;
+    try{
+        emailed = await ses.sendEmail(params).promise();
+    }
+    catch(err){
+        emailed = err;
+    }
+    return emailed;
 };
